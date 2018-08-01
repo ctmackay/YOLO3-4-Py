98db75038e03b411ad27102ad0be5b36e89521c9
ctmackay <charlestmackay@gmail.com>
2018-07-31 19:13:51 -0700

    This works

--- rtsp_detected_demo.py
+++ rtsp_detected_demo.py
@@ -6,6 +6,7 @@ import cv2
 print (cv2.getBuildInformation())
 import sys
 
+webcam = 0
 sample_vid = '/mnt/wd1tb/videos/nvidia/vlc-record-2017-08-04-21h10m29s-walsh_santomas_20170602_029.mp4-.mp4'
 nsd_46_stream = "rtsp://12.8.0.110:1935/birdseye/nsd_46.stream"
 fps = 5.0
@@ -28,8 +29,8 @@ if __name__ == "__main__":
     net = Detector(bytes(cfg_file, encoding="utf-8"), bytes(weights_file, encoding="utf-8"), 0,
                    bytes(data_file, encoding="utf-8"))
 
-    cap = cv2.VideoCapture(nsd_46_stream)
-    out = cv2.VideoWriter(wowza_pipeline, 0, fps, (1280, 720))
+    cap = cv2.VideoCapture(webcam)
+    out = cv2.VideoWriter(monitor_pipeline, 0, 30, (640, 480))
     time.sleep(5)
 
     if (not cap.isOpened()):
@@ -64,7 +65,7 @@ if __name__ == "__main__":
                 cv2.putText(frame, str(cat.decode("utf-8")), (int(x), int(y)), cv2.FONT_HERSHEY_COMPLEX, 1,
                             (255, 255, 0))
 
-            # cv2.imshow("preview", frame)
+            #cv2.imshow("preview", frame)
             out.write(frame)
             time.sleep(0.001)
 
